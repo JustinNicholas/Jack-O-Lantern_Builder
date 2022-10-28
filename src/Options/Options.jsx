4958b97a1d993404131540818fe2893a0c5494cf
Justin Nicholas <nicholasj964@yahoo.com>
2022-10-28 12:26:54 -0500

    updated pictures

--- src/Options/Options.jsx
+++ src/Options/Options.jsx
@@ -107,36 +107,6 @@ function Options() {
 
     if (isEditing === 0) {
     return(
-
-            //     <select onChange={(event) => addPumpkin(event)}>
-            //     <option value="none">select a pumpkin</option>
-            //     <option value="Black_Fat.png">Wide Black Pumpkin</option>
-            //     <option value="Green_Fat.png">Wide Green Pumpkin</option>
-            //     <option value="Pink_Fat.png">Wide Pink Pumpkin</option>
-            //     <option value="Orange_Fat.png">Wide Orange Pumpkin</option>
-            //     <option value="Black_Pear.png">Gord Black Pumpkin</option>
-            //     <option value="Green_Pear.png">Gord Green Pumpkin</option>
-            //     <option value="Pink_Pear.png">Gord Pink Pumpkin</option>
-            //     <option value="Orange_Pear.png">Gord Orange Pumpkin</option>
-            //     <option value="Black_Simple.png">Simple Black Pumpkin</option>
-            //     <option value="Green_Simple.png">Simple Green Pumpkin</option>
-            //     <option value="Pink_Simple.png">Simple Pink Pumpkin</option>
-            //     <option value="Orange_Simple.png">Simple Orange Pumpkin</option>
-            //     <option value="Black_Tall.png">Tall Black Pumpkin</option>
-            //     <option value="Green_Tall.png">Tall Green Pumpkin</option>
-            //     <option value="Pink_Tall.png">Tall Pink Pumpkin</option>
-            //     <option value="Orange_Tall.png">Tall Orange Pumpkin</option>
-            //   </select>
-            //   <select onChange={(event) => addEyes(event)}>
-            //     <option value="none">select the Eyes</option>
-            //     <option value="Traditional.png">Traditional Eyes</option>
-            //     <option value="angry.png">Angry Eyes</option>
-            //     <option value="cartoon.png">Cartoon Eyes</option>
-            //     <option value="Circle.png">Circle Eyes</option>
-            //   </select>
-            //   <button onClick={() => handleDownload()}>
-            //     Export As PNG
-            //   </button>
         <div className='options-container'>
         <div className='options-zone'>
           <div className='shape-option option-button' onClick={() => changeEditing(1)}>
@@ -177,7 +147,7 @@ function Options() {
                 <img src={process.env.PUBLIC_URL + '/Images/Gourd.png'} alt="" />
               </div>
               <div className='option-button' onClick={() => changeEditing(0)}>
-                <p>back</p>
+                <p className='no-margin'>Back</p>
               </div>
             </div>
           </div>
@@ -199,7 +169,7 @@ function Options() {
                 <img src={process.env.PUBLIC_URL + '/Images/Black.png'} alt="" />
               </div>
               <div className='option-button' onClick={() => changeEditing(0)}>
-                <p>back</p>
+                <p className='no-margin'>Back</p>
               </div>
             </div>
           </div>
@@ -239,7 +209,7 @@ function Options() {
                 <img className='features' src={process.env.PUBLIC_URL + '/Images/Eyes-10.png'} alt="" />
               </div>
               <div className='option-button' onClick={() => changeEditing(0)}>
-                <p className='no-margin'>back</p>
+                <p className='no-margin'>Back</p>
               </div>
             </div>
           </div>
@@ -279,7 +249,7 @@ function Options() {
                 <img className='features' src={process.env.PUBLIC_URL + '/Images/Nose-10.png'} alt="" />
               </div>
               <div className='option-button' onClick={() => changeEditing(0)}>
-                <p className='no-margin'>back</p>
+                <p className='no-margin'>Back</p>
               </div>
             </div>
           </div>
@@ -319,7 +289,7 @@ function Options() {
                 <img className='features' src={process.env.PUBLIC_URL + '/Images/Mouth-10.png'} alt="" />
               </div>
               <div className='option-button' onClick={() => changeEditing(0)}>
-                <p className='no-margin'>back</p>
+                <p className='no-margin'>Back</p>
               </div>
             </div>
           </div>
